f853279db4ccd57bf26a30a992b4266405322f1f
Mark Pim <j.mark.pim@gmail.com>
2016-10-25 21:01:03 +0100

    Initial commit of source code

---
+++ Parser/HCL/Pos.cs
@@ -0,0 +1,55 @@
+﻿namespace hcl_net.Parser.HCL
+{
+    class Pos
+    {
+        private readonly string _filename;
+        private readonly int _offset;
+        private readonly int _line;
+        private readonly int _column;
+
+        public Pos(string filename, int offset, int line, int column)
+        {
+            _filename = filename;
+            _offset = offset;
+            _line = line;
+            _column = column;
+        }
+
+        public string Filename
+        {
+            get { return _filename; }
+        }
+
+        public int Offset
+        {
+            get { return _offset; }
+        }
+
+        public int Line
+        {
+            get { return _line; }
+        }
+
+        public int Column
+        {
+            get { return _column; }
+        }
+
+        public bool IsValid()
+        {
+            return _line > 0;
+        }
+
+        public override string ToString()
+        {
+            var s = _filename;
+            if (IsValid())
+            {
+                s += string.IsNullOrEmpty(s) 
+                    ? "" : ":"
+                  + _line + ":" + _column;
+            }
+            return string.IsNullOrEmpty(s) ? "-" : s;
+        }
+    }
+}
---
+++ Parser/HCL/Scanner.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hcl_net.Parser.HCL
+{
+    class Scanner
+    {
+        private const char Eof = (char)0;
+
+        private readonly char[] _buf;
+    }
+}
