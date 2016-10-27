0334cb06b4fbb73ee53f89d46bf9ed5607b957c1
Mark Pim <j.mark.pim@gmail.com>
2016-10-27 23:07:54 +0100

    Send results to Coveralls.io

---
+++ afterTests.cmd
@@ -0,0 +1,7 @@
+nuget install NUnit.Runners -Version 2.6.4 -OutputDirectory tools
+nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
+nuget install coveralls.net -Version 0.412.0 -OutputDirectory tools
+ 
+.\tools\OpenCover.4.6.519\tools\OpenCover.Console.exe -target:.\tools\NUnit.Runners.2.6.4\tools\nunit-console.exe -targetargs:"/nologo /noshadow .\hcl-net.Test\bin\Debug\hcl-net.Test.dll" -filter:"+[*]* -[*.Test]*" -register:user
+
+.\tools\coveralls.net.0.412\tools\csmacnz.Coveralls.exe --opencover -i .\results.xml 
