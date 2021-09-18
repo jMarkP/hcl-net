f073173ee1d290e640b7e8a7b33048cc65e19b3d
Mark Pim <j.mark.pim@gmail.com>
2021-09-18 16:53:54 +0100

    Remove old implementation

--- v2/hclsyntax/parser/AST/INode.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace hcl_net.v2.hclsyntax.parser.AST
-{
-    internal delegate bool WalkFunc(INode node, out INode rewritten);
-    interface INode
-    {
-        Pos Pos { get; }
-        INode Walk(WalkFunc fn);
-    }
-}
